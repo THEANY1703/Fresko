3c9c82e1e240ebbd15069bf5eab3cc5c326920c8
Flavio Leotta <31816621+THEANY1703@users.noreply.github.com>
2024-12-18 19:27:18 +0100

    fix 41

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -17,7 +17,7 @@ import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
 import sign_in_with_apple
-import sqflite_darwin
+import sqflite
 import url_launcher_macos
 import video_player_avfoundation
 
